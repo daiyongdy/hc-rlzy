d71fbfeda6a08ecb5d8b7f81f2abc7f4c3dd15bd
daiyong <daiyong@coohua.com>
2018-03-22 22:03:44 +0800

    fix

--- src/main/java/com/cd/hc/Bootstrap.java
+++ src/main/java/com/cd/hc/Bootstrap.java
@@ -9,6 +9,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Bootstrap {
 
 	public static void main(String[] args) {
+
+
 		SpringApplication.run(Bootstrap.class, args);
 	}
 
